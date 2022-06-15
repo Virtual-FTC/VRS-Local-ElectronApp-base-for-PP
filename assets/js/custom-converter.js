c1ab6a80c2f313ad0046805875351f7fb2aff1eb
Joe <firehawkdigitalohio@gmail.com>
2022-06-15 15:57:29 +0800

    Update custom-converter.js

--- assets/js/custom-converter.js
+++ assets/js/custom-converter.js
@@ -14,18 +14,16 @@ const removeWordsJAVA = [
     "DcMotor.class,", "ColorSensor.class,", "(Double)", ".doubleValue()", ".toString()", "Double.parseDouble", "(DistanceSensor)"
 ]
 
-const replaceJSString = [
-    [": ElapsedTime;", " = null;"]
-    , [" : DcMotor;", " = null;"]    
-    , [": number;", " = null;"]
-    ,[": number ", ""]
+const gamepadBoxVars = {
+    a: 0, b: 1, c: 2, d: 3
+}
 
-    , ["{}", "{\n}"]
-    
+const replaceJSString = [
+    ,[": number ", ""]
+    , ["{}", "{\n}"]    
     , ['opModeIsActive', 'linearOpMode.opModeIsActive']
     , ['Range.clip(', 'range.clip(']
-    , ["ColorSensor, ", ""]
-    , ["JavaUtil.formatNumber(", "misc.formatNumber("]
+    // , ["JavaUtil.formatNumber(", "misc.formatNumber("]
 ]
 
 const modeTypes = ["LinearOpMode", "OpMode"]
@@ -52,6 +50,8 @@ var mortorVars = {}
 var colorVars = {}
 var elapsedTimeVars = {}
 var accelerateVars = {}
+
+var normalizedColors = {}
 
 
 
@@ -130,7 +130,7 @@ const valueConverter = (str) => {
         let value = getBracketContent(sides[1])
         return `colorSensor.getDistance(${colorData[colorIndex]}, ${value})`;
 
-    }else if(/gamepad(\d+).(\w+)_stick_(\w)/.test(str)){
+    }else if(/gamepad(\d+)\.(\w+)_stick_(\w)/.test(str)){
         const gamepadV = /gamepad(\d+).(\w+)_stick_(\w)/.exec(str)
         const keyV = `${gamepadV[2]}_stick_${gamepadV[3]}`
         let returnStr = ""
@@ -140,46 +140,106 @@ const valueConverter = (str) => {
             returnStr =  `gamepad.boolValue(${gamepadV[1]-1}, ${gamepadValues[keyV]}, 'Both')`
         return returnStr
 
-    }else if(str.includes(".getPower()")){
+    }else if(/gamepad(\d+).(a|b|c|d)/.test(str)){
+        const values = /gamepad(\d+).(a|b|c|d)/.exec(str)
+
+        return `gamepad.boolValue(${values[1]-1}, ${gamepadBoxVars[values[2]]}, 'Xbox')`
+    }
+    
+    else if(str.includes(".getPower()")){
         const exeVars = /this.(\w+).getPower/.exec(str)
         let returnStr = str.replace("()", "")
         return returnStr.replaceAll(/this.(\w+).getPower/g, `motor.getProperty(${mortorVars[exeVars[1]]}, 'Power')`)
-
-    }else if(str.includes("DistanceUnit.CM")){
+    }
+    else if(str.includes(".getGain()")){
+        const exeVars = /\(?this.(\w+)\)?.getGain\(\)/.exec(str)
+        return str.replaceAll(/\(?this.(\w+)\)?.getGain\(\)/g, `colorSensor.getProperty(${colorVars[exeVars[1]]}, "Gain")`)
+    }
+    else if(str.includes(".getNormalizedColors()")){
+        const exeVars = /\(?this\.(\w+)\)?.getNormalizedColors\(\)/.exec(str)
+        return str.replaceAll(/\(?this\.(\w+)\)?.getNormalizedColors\(\)/g, `JSON.parse(colorSensor.getNormalizedColors(${colorVars[exeVars[1]]}))`)
+    }
+    else if(str.includes("DistanceUnit.")){
         const exeVars = /DistanceUnit.(\w+)/g.exec(str)
         return str.replaceAll(`DistanceUnit.${exeVars[1]}`, `'${exeVars[1]}'`)
     }
 
-    else if(/(\w+).toString\(\)/g.test(str)){
+    else if(/(\w+)\.toString\(\)/g.test(str)){
         let values = /(\w+).toString\(\)/g.exec(str)
         if(elapsedTimeVars[values[1]]){
             return str.replace(/(\w+).toString\(\)/g, `String(elapsedTime.toText(${values[1]}))`)
         }
     }
 
-    else if(/(\w+).seconds\(\)/g.test(str)){
+    else if(/(\w+)\.seconds\(\)/g.test(str)){
         let values = /(\w+).seconds\(\)/g.exec(str)
         if(elapsedTimeVars[values[1]]){
             return str.replace(/(\w+).seconds\(\)/g, `String(elapsedTime.get("Seconds", ${values[1]}))`)
         }
     }
 
-    else if(/(\w+).reset\(\)/g.test(str)){
+    else if(/(\w+)\.reset\(\)/g.test(str)){
         let values = /(\w+).reset\(\)/g.exec(str)
         if(elapsedTimeVars[values[1]]){
             return str.replace(/(\w+).reset\(\)/g, `elapsedTime.reset(${values[1]})`)
         }
     }
 
-    else if(/(\w+).toUnit\((\w+)\)/g.test(str)){
+    else if(/(\w+)\.toUnit\((\w+)\)/g.test(str)){
         let values = /(\w+).toUnit\((\w+)\)/g.exec(str)
         if(accelerateVars[values[1]]){
             return str.replace(/(\w+).toUnit\((\w+)\)/g, `acceleration.toDistanceUnit(${values[1]}, "${values[2]}")`)
         }
     }
 
+    else if(str.includes(".getLightDetected()")){
+        const values = /\(?this.(\w+)\)?.getLightDetected\(\)/g.exec(str)
+        return str.replace(/\(?this.(\w+)\)?.getLightDetected\(\)/g, `colorSensor.getProperty(${colorVars[values[1]]}, "LightDetected")`)
+    }
+    // else if(/misc.formatNumber\((\w+).(\w+), (\d+)\)/.test(str)){
+    else if(/\bmisc\.formatNumber\((\w+)\.(\w+),/.test(str)){
+        const values = /misc.formatNumber\((\w+).(\w+),/.exec(str)
+        return str.replace(/misc.formatNumber\((\w+).(\w+),/, `misc.roundDecimal(colorUtil.normalized("${capitalize(values[2])}", ${values[1]}),`)
+    }
+
+    else if(/\bmisc\.colorToValue\((\w+)\)/.test(str)){
+        const values = /misc\.colorToValue\((\w+)\)/.exec(str)
+        return str.replace(/misc.colorToValue\((\w+)\)/, `colorUtil.get("Hue", ${values[1]})`)
+    }
+
+
+    else if(/(\w+)\.toColor\(\)/.test(str)){
+        const values = /(\w+).toColor\(\)/.exec(str)
+        return str.replace(/(\w+).toColor\(\)/, `colorUtil.normalized("Color", ${values[1]})`)
+    }    
+    
+    else if(/\bmisc.colorToHue\((\w+)\)/.test(str)){
+        const values = /\bmisc.colorToHue\((\w+)\)/.exec(str)
+        return str.replace(/\bmisc.colorToHue\((\w+)\)/, `colorUtil.get("Hue", ${values[1]})`)
+    }
+
+    else if(/\bmisc\.colorToSaturation\((\w+)\)/.test(str)){
+        const values = /\bmisc\.colorToSaturation\((\w+)\)/.exec(str)
+        return str.replace(/\bmisc\.colorToSaturation\((\w+)\)/, `colorUtil.get("Saturation", ${values[1]})`)
+    }
+
+    else if(/\bmisc.formatNumber\(/.test(str)){
+        return str.replace(/\bmisc.formatNumber\(/, "misc.roundDecimal(")
+    }
+
+    else if(/\bColor\.parseColor\("(\w+)"\)/.test(str)){
+        const values = /\bColor\.parseColor\("(\w+)"\)/.exec(str)
+        console.log("color values : ", values)
+        return str.replace(/\bColor\.parseColor\("(\w+)"\)/, `colorUtil.textToColor('${values[1]}')`)
+    }
+
+    
     
     return str
+}
+const capitalize = (str) => {
+    const lower = str.toLowerCase();
+    return str.charAt(0).toUpperCase() + lower.slice(1);
 }
 const valueChecker = (str) => {
     if (str.includes("JavaUtil.inListGet(")) {
@@ -208,12 +268,12 @@ const valueChecker = (str) => {
 const customConvert = (str) => {
     let result = str;
     if (result.includes('hardwareMap.get')) {
-        let hardmaps = /this.(\w+) = hardwareMap.get\(DcMotor, "(\w+)"\);/g.exec(result);
+        let hardmaps = /this.(\w+) = hardwareMap.get\((\w+), "(\w+)"\);/g.exec(result);
         const varName = hardmaps[1];
-        const varValue = hardmaps[2];
-        if (directions[varValue] != undefined)
+        const varValue = hardmaps[3];
+        if (hardmaps[2] == 'DcMotor')
             mortorVars[varName] = directions[varValue];
-        else if (colorData[varValue] != undefined)
+        else if (hardmaps[2] == 'ColorSensor')
             colorVars[varName] = colorData[varValue];
         return "";
     }
@@ -275,10 +335,22 @@ const customConvert = (str) => {
         const value = valueChecker(matches[2]);
         return `motor.setProperty([${mortorVars[varName]}], 'TargetPositionTolerance', [${value?value:0}]);`;
     }
+
+    else if(str.includes(".setGain(")){
+        const values = /\(?this.(\w+)\)?.setGain\((\w+)\)/.exec(str)
+        return str.replace(/\(?this.(\w+)\)?.setGain\((\w+)\)/, `colorSensor.setProperty(${colorVars[values[1]]}, "Gain", ${values[2]})`);
+    }
+
+
+    else if(/\bmisc\.showColor\((.*), (.*)\);/.test(str)){
+        const values = /\bmisc\.showColor\((.*), (.*)\);/.exec(str)
+        return str.replace(/\bmisc\.showColor\((.*), (.*)\);/, `colorUtil.showColor( ${valueChecker(values[2])});`)
+    }
+
     else if (str.includes("if (")) {
         let sides = str.split("if (");
         const value = valueChecker(sides[1].split(") {")[0]);
-        return `if (${value}) {` + sides[1].split(") {")[1];
+        return sides[0] + `if (${value}) {` + sides[1].split(") {")[1];
     }
     else if (str.includes("JavaUtil.createListWith(")) {
         let sides = str.split("JavaUtil.createListWith(");
@@ -348,9 +420,19 @@ async function convert_2js(url, javaCode, callback) {
 
             })
 
+        
+
         replaceJSString.map(word => {
             result = result.replaceAll(word[0], word[1])
         })
+
+        //Total remove vars
+        result = result.replaceAll(/: (\w+);/g, " = null;")
+        result = result.replaceAll(/<(\w+)>/g, "")
+        result = result.replaceAll(/\bparseFloat\b/g, "")
+        result = result.replaceAll(/\bJavaUtil./g, "misc.")
+        
+
 
         if(/export class (\w+) extends LinearOpMode\b/g.test(result)){
             OpMode = "LinearOpMode"
@@ -363,6 +445,12 @@ async function convert_2js(url, javaCode, callback) {
         result = result.split('\n');
         for (let i = 1; i < result.length; i++) {
             lineTxt = result[i].trim();
+            var middleVars = /\bDistanceUnit.(\w+)/g.exec(lineTxt)
+            // partial remove vars
+            if(middleVars){
+                lineTxt = lineTxt.replace(/\bDistanceUnit.(\w+)/g, `"${middleVars[1]}"`)
+            }
+
             brackets += checkBrackets(lineTxt);
             // var 
             if (brackets == 1 && !funcName) {
